0bf0f6675f74ff8e359fd1ba62fb9178a7859900
rajeshKumarbit <banyalrajesh20@gmail.com>
2025-09-21 05:26:44 +0530

    Edited UserManager and User Classes

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,51 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class UserManager(BaseUserManager):
-    pass
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+        if not username:
+            raise ValueError("User must have an username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,  first_name, last_name, username, email, password=None):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            password=password,
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
 
 
 class User(AbstractBaseUser):
-    pass 
+    RESTAURANT = 1
+    CUSTOMER = 2
+
+    ROLE_CHOICE = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer'),
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.EmailField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICE, blank=True, null=True)
 
